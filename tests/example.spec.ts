fec7fdc1977c58fdf83dc22c0b3f7f63431f81fd
Autom8e <hppynmrrd@gmail.com>
2025-09-17 23:26:27 +0300

    chore: Decreased the number of tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,32 +1,32 @@
-import {test, expect} from '@playwright/test';
-
-test('has title', async ({page}) => {
-    await page.goto('https://playwright.dev/');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({page}) => {
-    await page.goto('https://playwright.dev/');
-
-    // Click the get started link.
-    await page.getByRole('link', {name: 'Get started'}).click();
-
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
-});
-
-test.beforeEach(async ({page}) => {
-    await page.goto('https://www.google.com');
-})
-
-test('description of the test', {
-    tag: '@first tag'
-}, async ({page}) => {
-    await page.goto('https://www.google.com');
-    const searchField = page.getByRole('combobox', { name: /ara/i });
-    await searchField.fill('first test case with tag')
-    await searchField.press('Enter')
-    await page.waitForTimeout(4000)
-})
+// import {test, expect} from '@playwright/test';
+//
+// test('has title', async ({page}) => {
+//     await page.goto('https://playwright.dev/');
+//
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle(/Playwright/);
+// });
+//
+// test('get started link', async ({page}) => {
+//     await page.goto('https://playwright.dev/');
+//
+//     // Click the get started link.
+//     await page.getByRole('link', {name: 'Get started'}).click();
+//
+//     // Expects page to have a heading with the name of Installation.
+//     await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+// });
+//
+// test.beforeEach(async ({page}) => {
+//     await page.goto('https://www.google.com');
+// })
+//
+// test('description of the test', {
+//     tag: '@first tag'
+// }, async ({page}) => {
+//     await page.goto('https://www.google.com');
+//     const searchField = page.getByRole('combobox', { name: /ara/i });
+//     await searchField.fill('first test case with tag')
+//     await searchField.press('Enter')
+//     await page.waitForTimeout(4000)
+// })
