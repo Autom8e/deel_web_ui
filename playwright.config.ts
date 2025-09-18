039a5a21002996e3fc29583f6c7e4d30cb9c2a80
Autom8e <hppynmrrd@gmail.com>
2025-09-18 17:56:38 +0300

    Video, trace, ss added to config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,9 +29,9 @@ export default defineConfig({
             /* Base URL to use in actions like `await page.goto('/')`. */
             // baseURL: 'http://localhost:3000',
             testIdAttribute: 'data-product-id',
-            /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-            trace:
-                'on-first-retry',
+            trace: 'retain-on-failure',
+            video: 'retain-on-failure',
+            screenshot: 'only-on-failure',
         }
     ,
 
